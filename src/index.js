c5bb9c7794b93bf652d9a010fdc60bdcc9d66965
CE61 <ce61@debian-BULLSEYE-live-builder-AMD64>
2022-06-18 14:24:40 -0400

    First commit with some preliminary work before functionality added to modules

---
+++ src/index.js
@@ -0,0 +1,32 @@
+"use strict";
+
+class Project{
+    constructor(title, description){
+        this.title = title;
+        this.description = description;
+        this.toDoItemArray = [];
+    }
+    checkCompletion(){
+        if(this.toDoItemArray.every((toDoItem)=>{toDoItem.isFinished===true})){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    addToDoItem(title, description, dueDate, priority, notes, isFinished){
+        const newToDoItem = new toDoItem(title, description, dueDate, priority, notes, isFinished);
+        this.toDoItemArray.push(newToDoItem);
+    }
+}
+class toDoItem{
+    constructor(title, description, dueDate, priority, notes, isFinished){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+        this.isFinished = false;
+    }
+}
+
+const projectList = [];
